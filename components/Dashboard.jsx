5ac3a31260d657a00f1cdd74b75350901696aa3f
Vldadimir Ilin <bettyboop200195@gmail.com>
2022-04-22 16:57:16 +0300

    my-clone-spotify

---
+++ components/Dashboard.jsx
@@ -0,0 +1,44 @@
+import Sidebar from "./Sidebar";
+import SpotifyWebApi from "spotify-web-api-node";
+import { useSession } from "next-auth/react";
+import Player from "./Player";
+import { playingTrackState } from "../atoms/playerAtom";
+import { useRecoilState } from "recoil";
+import Body from "./Body";
+import Right from "./Right";
+import { useState, useEffect } from "react";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
+
+function Dashboard() {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken;
+
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  useEffect(() => {
+    setShowPlayer(true);
+  }, []);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
+
+  return (
+    <main className="flex min-h-screen min-x-max bg-black lg:pb-24">
+      <Sidebar />
+      <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+      <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+      {showPlayer && (
+        <div className="fixed bottom-0 left-0 right-0 z-50">
+          <Player accessToken={accessToken} trackUri={playingTrack.uri} />
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default Dashboard;
